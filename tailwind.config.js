8b06fb6a0833a1e055041e8f0a8d11d89b3796e5
sergsagan <sergey.martynuk@gmail.com>
2024-08-29 14:04:06 +0200

    create header

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,ts,js,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
