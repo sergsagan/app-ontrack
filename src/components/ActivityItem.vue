6f6003db8d4087bfbbb70a9a6e2705948418131e
sergsagan <sergey.martynuk@gmail.com>
2024-09-05 11:36:37 +0200

    display of remaining time

--- src/components/ActivityItem.vue
+++ src/components/ActivityItem.vue
@@ -2,8 +2,10 @@
 import { TrashIcon } from '@heroicons/vue/24/outline/index.js'
 import BaseSelect from './BaseSelect.vue'
 import BaseButton from './BaseButton.vue'
+import ActivitySecondsToComplete from './ActivitySecondsToComplete.vue'
 import { PERIOD_SELECT_OPTIONS, BUTTON_TYPE_DANGER } from '@/constans.js'
 import { isActivityValid, isUndefined, isNumber } from '@/validators.js'
+
 
 defineProps({
   activity: {
@@ -30,13 +32,17 @@ const emit = defineEmits({
       </BaseButton>
       <span class="truncate text-xl">{{ activity.name }}</span>
     </div>
-    <div>
+    <div class="flex gap-2">
       <BaseSelect
-        class="font-mono"
+        class="grow font-mono"
         :selected="activity.secondsToComplete || null"
         :options="PERIOD_SELECT_OPTIONS"
         placeholder="hh:mm"
         @select="emit('setSecondsToComplete', $event || 0)"
+      />
+      <ActivitySecondsToComplete
+        v-if="activity.secondsToComplete"
+        :activity="activity"
       />
     </div>
   </li>
---
+++ src/components/ActivitySecondsToComplete.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { formatSeconds } from '@/function.js'
+import { isActivityValid } from '@/validators.js'
+
+defineProps({
+  activity: {
+    type: Object,
+    required: true,
+    validator: isActivityValid
+  }
+})
+</script>
+
+<template>
+  <div class="flex items-center rounded bg-purple-100 px-2 font-mono text-xl text-purple-600">
+    {{ formatSeconds(activity.secondsToComplete) }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
